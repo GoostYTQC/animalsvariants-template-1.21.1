6f1dfd169f817f6e7dab2d718f41e7b8bd28213a
Elden <Elden@homelab.local>
2025-02-20 12:43:00 -0500

    Initial commit

---
+++ src/main/java/com/github/goostytqc/AnimalsVariants.java
@@ -0,0 +1,16 @@
+package com.github.goostytqc;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class AnimalsVariants implements ModInitializer {
+	public static final String MOD_ID = "animalsvariants";
+
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+	}
+}
